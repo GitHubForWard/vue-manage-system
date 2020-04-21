b7c6d89222e7eed2c4824c1696673d6266f695ef
王向前 <95150209@qq.com>
2020-04-21 15:09:29 +0800

    用户管理页面增加表格显示

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,5 +1,6 @@
 import Mock from "mockjs";
 import homeApi from "./home";
+import userApi from "./user";
 
 // 设置200-2000的延迟
 Mock.setup({
@@ -7,3 +8,8 @@ Mock.setup({
 })
 
 Mock.mock(/\/home\/getData/, 'get', homeApi.getStatisticalData);
+Mock.mock(/\/user\/getUser/, 'get', userApi.getUserList);
+Mock.mock(/\/user\/delete/, 'get', userApi.deleteUser)
+Mock.mock(/\/user\/batchremove/, 'get', userApi.batchremove)
+Mock.mock(/\/user\/add/, 'post', userApi.createUser)
+Mock.mock(/\/user\/edit/, 'post', userApi.updateUser)
--- src/views/pages/UserManage.vue
+++ src/views/pages/UserManage.vue
@@ -1,13 +1,63 @@
 <template>
-  <div>user</div>
+  <div>
+    <template>
+      <el-table :data="tableData" style="width: 100%" >
+        <el-table-column type="index" width="50"></el-table-column>
+        <el-table-column
+          v-for="(item, key) in tableLabel"
+          :key="key"
+          :prop="item.prop"
+          :label="item.label"
+        ></el-table-column>
+      </el-table>
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      tableData: [],
+      tableLabel: [
+        {
+          prop: "name",
+          label: "姓名"
+        },
+        {
+          prop: "age",
+          label: "年龄"
+        },
+        {
+          prop: "sexLabel",
+          label: "性别"
+        },
+        {
+          prop: "birth",
+          label: "出生日期",
+          width: 200
+        },
+        {
+          prop: "addr",
+          label: "地址",
+          width: 320
+        }
+      ]
+    };
+  },
+  methods: {
+    getUserList() {
+      this.$http.get("/user/getUser").then(res => {
+        console.log("userData", res);
+        this.tableData = res.data.list;
+      });
+    }
+  },
+  mounted() {
+    this.getUserList();
+  }
+};
 </script>
 
 <style>
-
 </style>
