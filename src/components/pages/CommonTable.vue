3fcf25833cdb20d0885092db5790ca258bfee25a
wangxiangqian <king_forward@sina.com>
2020-04-22 22:52:15 +0800

    table表格增加分液器

--- src/components/pages/CommonTable.vue
+++ src/components/pages/CommonTable.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="common-table">
-    <el-table :data="tableData">
-      <el-table-column label="序号">
+    <el-table :data="tableData" stripe v-loading="config.loading">
+      <el-table-column label="序号" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.date }}</span>
+          <span>{{ (config.page - 1) * 20 + scope.$index + 1 }}</span>
         </template>
       </el-table-column>
 
-      <el-table-column v-for="item in tableLabel" :key="item.prop" :label="item.label">
+      <el-table-column
+        align="center"
+        v-for="item in tableLabel"
+        :key="item.prop"
+        :label="item.label"
+      >
         <template slot-scope="scope">
           <span>{{ scope.row[item.prop] }}</span>
         </template>
       </el-table-column>
+
+      <el-table-column align="center" label="操作" width="180">
+        <template>
+          <el-button size="mini">编辑</el-button>
+          <el-button size="mini" type="danger">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="config.total"
+      :current-page.sync="config.page"
+      @current-change="changePage"
+    ></el-pagination>
   </div>
 </template>
 
@@ -22,9 +41,18 @@ export default {
     tableData: Array,
     tableLabel: Array,
     config: Object
+  },
+  methods: {
+    changePage(page) {
+      this.$emit("changePage", page);
+    }
   }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.el-pagination {
+  text-align: right;
+  margin-top: 24px;
+}
 </style>
--- src/views/pages/UserManage.vue
+++ src/views/pages/UserManage.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="manage">
-    <div class="manage-form">
-      <el-button type="primary">
-        <i class="el-icon-plus"></i>新增
-      </el-button>
-      <common-form inline :formLabel="formLabel" :form="searchFrom">
-        <el-button type="primary">
-          <i class="el-icon-search"></i>搜索
-        </el-button>
-      </common-form>
-    </div>
     <el-card shadow="hover">
-      <common-table :tableData="tableData" :tableLabel="tableLabel" :config="config"></common-table>
+      <div class="manage-form">
+        <el-button type="primary">
+          <i class="el-icon-plus"></i>新增
+        </el-button>
+        <common-form inline :formLabel="formLabel" :form="searchFrom">
+          <el-button type="primary">
+            <i class="el-icon-search"></i>搜索
+          </el-button>
+        </common-form>
+      </div>
+      <common-table
+        :tableData="tableData"
+        :tableLabel="tableLabel"
+        :config="config"
+        @changePage="getUserList"
+      ></common-table>
     </el-card>
   </div>
 </template>
@@ -71,15 +76,24 @@ export default {
   methods: {
     getUserList() {
       this.config.loading = true;
-      this.$http.get("/user/getUser").then(res => {
-        this.config.loading = false;
-        this.tableData = res.data.list.map(item => {
-          item.sexLabel = item.sex === 0 ? "女" : "男";
-          return item;
+      this.$http
+        .get("/user/getUser", {
+          params: {
+            page: this.config.page
+          }
+        })
+        .then(res => {
+          this.config.loading = false;
+          this.tableData = res.data.list.map(item => {
+            item.sexLabel = item.sex === 0 ? "女" : "男";
+            return item;
+          });
+          this.config.total = res.data.count;
+          console.log(this.tableData, this.config.total);
         });
-        this.config.total = res.data.count;
-        console.log(this.tableData, this.config.total);
-      });
+    },
+    changePage(page) {
+      console.log(page);
     }
   },
   mounted() {
@@ -90,6 +104,7 @@ export default {
 
 <style lang="scss" scoped>
 .manage {
+  height: 100%;
   .manage-form {
     display: flex;
     align-items: center;
